2399b9e302a5c82d8ccfd32537c4a35dd5ab2b09
takuyahori <takuyahori0904@gmail.com>
2022-06-02 21:35:52 +0900

    必要ファイル作成

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -2,7 +2,10 @@ import React from 'react';
 import firstViewImg from "../images/23842768_s.jpg";
 
 
-const FirstView = () => {
+const HomePage = () => {
+
+  
+
   return (
     <div className='firstView'>
         <div className='firstView__img' style={{
@@ -16,4 +19,4 @@ const FirstView = () => {
   )
 }
 
-export default FirstView
+export default HomePage

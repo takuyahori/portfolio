29d03b9f592e6831b26ca6cc08c8470d01607f3a
takuyahori <takuyahori0904@gmail.com>
2022-06-02 17:55:14 +0900

    headerデザイン記述

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className='logo'>
+            <h3>Portfolio</h3>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>        
+                </li>
+                <li>
+                    <a href="#">About</a>        
+                </li>
+                <li>
+                    <a href="#">Products</a>        
+                </li>
+                <li>
+                    <a href="#">Contact</a>        
+                </li>
+            </ul>
+        </nav>
+    </header>
   )
 }
 

bebba75e183426c812f6f3240ba4bc2dd8c97cd6
takuyahori <takuyahori0904@gmail.com>
2022-06-03 19:53:35 +0900

    formデザイン記述

--- src/component/ContactPage.js
+++ src/component/ContactPage.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <section className="contact">
+      <h1>Contact Form</h1>
+      <hr />
+      <div className='contact__wrap'>
+        <div className='contact__left'>
+          <form className='contact__form'>
+            <div className='contact__field'>
+              <label>Name</label>
+              <input type='text' name='name'></input>
+            </div>
+          </form>
+          <form className='contact__form'>
+            <div className='contact__field'>
+              <label>Mail</label>
+              <input type='text' name='mailAddress'></input>
+            </div>
+          </form>
+        </div>
+        <div className='contact__right'>
+          <form className='contact__form'>
+            <div className='contact__field'>
+              <label>Message</label>
+              <input type='text' name='message'></input>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className='contact__send'>
+         <button>send</button>
+      </div>
+    </section>
   )
 }
 

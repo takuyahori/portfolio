d9e87d9d7d4cba50a202805808adf2acce705974
takuyahori <takuyahori0904@gmail.com>
2022-06-04 09:10:49 +0900

    emailjs API実装

--- src/component/ContactPage.js
+++ src/component/ContactPage.js
@@ -1,37 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { send } from 'emailjs-com';
 
 const ContactPage = () => {
+
+  const [ name, setName ] = useState("");
+  const [ mailAddress, setMailAddress ] = useState("");
+  const [ message, setMessage ] = useState("");
+
+  
+  const serviceID = process.env.REACT_APP_SERVICE_ID;
+  const templateID = process.env.REACT_APP_TEMPLATE_ID;
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleMail = (e) => {
+    setMailAddress(e.target.value)
+  }
+
+  const handleMessage = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const sendMail = (e) => {
+    e.preventDefault();
+    send(
+      'service_cjhg1c8',
+      'template_u999xnh',
+      { name, mailAddress, message },
+      'eWFO3EkLuVxWTVWq5'
+    )
+    .then((response) => {
+      console.log('Message sent successfully', response.status, response.text)
+    })
+    .catch((err) => {
+      console.log('Failed', err)
+    })
+  }
+
   return (
     <section className="contact">
       <h1>Contact Form</h1>
       <hr />
-      <div className='contact__wrap'>
-        <div className='contact__left'>
-          <form className='contact__form'>
+      <form className='contact__form' onSubmit={sendMail}>
+        <div className='contact__wrap'>
+          <div className='contact__left'>
             <div className='contact__field'>
               <label>Name</label>
-              <input type='text' name='name'></input>
+              <input
+                type='text' 
+                name='name'
+                value={name}
+                onChange={handleName}
+              />
             </div>
-          </form>
-          <form className='contact__form'>
             <div className='contact__field'>
               <label>Mail</label>
-              <input type='text' name='mailAddress'></input>
+              <input
+                type='text' 
+                name='mailAddress' 
+                value={mailAddress}
+                onChange={handleMail}
+              />
             </div>
-          </form>
-        </div>
-        <div className='contact__right'>
-          <form className='contact__form'>
+          </div>
+          <div className='contact__right'>
             <div className='contact__field'>
               <label>Message</label>
-              <input type='text' name='message'></input>
+              <input 
+                type='text' 
+                name='message'
+                value={message}
+                onChange={handleMessage}
+              />
             </div>
-          </form>
+          </div>
         </div>
-      </div>
-      <div className='contact__send'>
-         <button>send</button>
-      </div>
+        <div className='contact__send'>
+          <button>send</button>
+        </div>
+      </form>
     </section>
   )
 }

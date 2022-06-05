31b3e51793b6cd6a1c40b826a558df7a6478e674
takuyahori <takuyahori0904@gmail.com>
2022-06-05 15:53:26 +0900

    バリデーション判定とsendメソッド連携

--- src/component/ContactPage.js
+++ src/component/ContactPage.js
@@ -3,42 +3,62 @@ import { send } from 'emailjs-com';
 
 const ContactPage = () => {
 
-  const [ name, setName ] = useState("");
-  const [ mailAddress, setMailAddress ] = useState("");
-  const [ message, setMessage ] = useState("");
+  /* input要素内に文字を入力したときの各属性値の空の変数 */
+  const initialValues = { name: "", mailAddress: "", message: "" };
 
-  
-  const serviceID = process.env.REACT_APP_SERVICE_ID;
-  const templateID = process.env.REACT_APP_TEMPLATE_ID;
-  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+  /* 上記の変数に値を格納していくためのuseState。入力するたびテキストが値として変数に格納される。 */
+  const [formValues, setFormValues] = useState(initialValues);
 
-  const handleName = (e) => {
-    setName(e.target.value)
-  }
+    /* 上記の各属性のエラーとそのメッセージ内容を格納するuseState */
+  const [formErrors, setFormErrors] = useState({});
+  const [ isSubmit, setIsSubmit ] = useState(false);
 
-  const handleMail = (e) => {
-    setMailAddress(e.target.value)
-  }
-
-  const handleMessage = (e) => {
-    setMessage(e.target.value)
-  }
+  /* フォームにテキストが入力されたら */
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
   const sendMail = (e) => {
     e.preventDefault();
-    send(
-      'service_cjhg1c8',
-      'template_u999xnh',
-      { name, mailAddress, message },
-      'eWFO3EkLuVxWTVWq5'
-    )
-    .then((response) => {
-      console.log('Message sent successfully', response.status, response.text)
-    })
-    .catch((err) => {
-      console.log('Failed', err)
-    })
-  }
+    //ログイン情報を送信する前にバリデーションチェック
+    /* errorsのuseStateの状態を更新するため。 formValuesに入った値がvalidate関数でエラーかどうか判断される*/
+    //setFormErrors(validate(formValues));
+    const err = validate(formValues);
+    if(Object.keys(err).length > 0) {
+      setFormErrors(err);
+      return;
+    }
+    setIsSubmit(true);
+    
+    /* メール送信処理 */
+    if(Object.keys(formErrors).length > 0) {
+      send(
+        'service_cjhg1c8',
+        'template_u999xnh',
+        { formValues },
+        'eWFO3EkLuVxWTVWq5'
+      )
+    };
+  };
+
+  /* 各項目バリデーションチェック関数 */
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/;
+    if(!values.name) {
+      errors.name = "名前を入力してください。";
+    }
+    if(!values.mailAddress) {
+      errors.mailAddress = "メールアドレスを入力してください。";
+    } else if(!regex.test(values.mailAddress)) {
+      errors.mailAddress = "正しいメールアドレスを入力してください。"
+    }
+    if(!values.message) {
+      errors.message = "メッセージを入力してください。";
+    }
+    return errors;
+  };
 
   return (
     <section className="contact">
@@ -52,19 +72,21 @@ const ContactPage = () => {
               <input
                 type='text' 
                 name='name'
-                value={name}
-                onChange={handleName}
+                value={formValues.name}
+                onChange={(e) => handleChange(e)}
               />
             </div>
+            <p className='contact__error'>{formErrors.name}</p>
             <div className='contact__field'>
               <label>Mail</label>
               <input
                 type='text' 
                 name='mailAddress' 
-                value={mailAddress}
-                onChange={handleMail}
+                value={formValues.mailAddress}
+                onChange={(e) => handleChange(e)}
               />
             </div>
+            <p className='contact__error'>{formErrors.mailAddress}</p>
           </div>
           <div className='contact__right'>
             <div className='contact__field'>
@@ -72,18 +94,22 @@ const ContactPage = () => {
               <input 
                 type='text' 
                 name='message'
-                value={message}
-                onChange={handleMessage}
+                value={formValues.message}
+                onChange={(e) => handleChange(e)}
               />
             </div>
+            <p className='contact__error'>{formErrors.message}</p>
           </div>
         </div>
         <div className='contact__send'>
           <button>send</button>
+          {Object.keys(formErrors).length > 0 && isSubmit && (
+            <div className='content__messageOk'>送信完了しました。</div>
+          )}
         </div>
       </form>
     </section>
   )
-}
+};
 
 export default ContactPage

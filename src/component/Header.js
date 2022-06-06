b8e64bfa50ed7e1d5beaf057bdb40421908d8e2d
takuyahori <takuyahori0904@gmail.com>
2022-06-06 15:19:50 +0900

    ハンバーガメニュー作成

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import { slide as Menu } from 'react-burger-menu';
 
 const Header = () => {
+
+  const [ active, setActive ] = useState(false);
+
+  const classToggle = () => {
+      setActive(!active)
+  };
+
   return (
     <header>
-        <div className='logo'>
-            <h3>Portfolio</h3>
+        <div className='header__logo'>
+            <i className="fa-solid fa-dumbbell"></i>
         </div>
 
-        <nav>
-            <ul>
-                <li>
+        <nav className='header__nav'>
+            <ul className={active ? "is-active" : ""}>
+                <li className={active ? "is-active" : ""}>
                     <Link to="/">Home</Link>        
                 </li>
-                <li>
+                <li className={active ? "is-active" : ""}>
                     <Link to="/about">About</Link>     
                 </li>
-                <li>
+                <li className={active ? "is-active" : ""}>
                     <Link to="/products">Products</Link>        
                 </li>
-                <li>
+                <li className={active ? "is-active" : ""}>
                     <Link to="/contact">Contact</Link> 
                 </li>
-                <li>
+                <li className={active ? "is-active" : ""}>
                     <a href="https://github.com/takuyahori"><i className="fa-brands fa-github"></i></a> 
                 </li>
             </ul>
+            <div className='header__burger' onClick={classToggle}>
+                <div className='header__burger--line1'></div>
+                <div className='header__burger--line2'></div>
+                <div className='header__burger--line3'></div>
+            </div>
         </nav>
     </header>
   )

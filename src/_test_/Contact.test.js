a1d081bd543a5d2697475c63c3c08a893cefe33a
takuyahori <takuyahori0904@gmail.com>
2022-06-09 16:08:56 +0900

    emailjs記述変更

--- src/_test_/Contact.test.js
+++ src/_test_/Contact.test.js
@@ -8,27 +8,4 @@ describe("Test ContactPage Component", () => {
         const button = await screen.findAllByRole("button");
         expect(button).toHaveLength(1);
     })
-
-    test("should be failed email validation", () => {
-        render(<ContactPage />);
-        const testEmail = "takuyahori.com";
-        expect(validate(testEmail)).not.toBe(true);
-    })
-
-    test("should be able to submit the form", () => {
-        render(<ContactPage />);
-        const submitButton = screen.getByTestId("submit");
-        const name = screen.getByPlaceholderText("name");
-        const email = screen.getByPlaceholderText("mail");
-        const message = screen.getByPlaceholderText("message");
-        
-        userEvent.type(name, "takuya");
-        userEvent.type(email, "takuyahori0904@gmail.com");
-        userEvent.type(message, "hello");
-
-        userEvent.click(submitButton)
-        const userInfo = screen.getByText(/送信完了しました。/i);
-        expect(userInfo).toBeInTheDocument();
-
-    })
 })
